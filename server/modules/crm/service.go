ad2aa5e572e68da0872c17e005e49ae413064738
mishhgun01 <golubev1400@mail.ru>
2024-06-10 19:25:30 +0300

    middleware

--- server/modules/crm/service.go
+++ server/modules/crm/service.go
@@ -38,3 +38,13 @@ func NewService(db repository.CRMRepo) *Service {
 func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.r.ServeHTTP(w, r)
 }
+
+func (s *Service) middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, SessionID")
+		next.ServeHTTP(w, r)
+	})
+}
